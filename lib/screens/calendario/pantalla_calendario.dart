f53189e9bc6cefd39469426a26fb6a0c165f57c1
LautyT02 <71523956+LautyT02@users.noreply.github.com>
2020-10-26 07:45:11 -0300

    Se añadieron modificaciones al calendario

--- lib/screens/calendario/pantalla_calendario.dart
+++ lib/screens/calendario/pantalla_calendario.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/Calendario/calendario.dart';
+import 'package:flutter_app/screens/calendario/crearEvento.dart';
+
+/*
+Esta es la página principal. Acá es donde se puede ver el calendario
+ */
+
+
 
 class PantallaCalendario extends StatefulWidget {
   @override
@@ -8,29 +15,11 @@ class PantallaCalendario extends StatefulWidget {
 }
 
 class _PantallaCalendarioState extends State<PantallaCalendario> {
+  //Esto almacena la fecha que llega de crear Evento
+  DateTime nuevo= new DateTime.now();
+  //Se crea un objeto de tipo calendario
   Calendario calendario=new Calendario();
 
-  /*List <Eventos> dia1 = [
-    Eventos(titulo: 'Cuestionario 1', hora: '19:30', lugar: 'Neuquén', frecuencia: 'diario'),
-    Eventos(titulo: 'Cuestionario 5', hora: '19:45', lugar: 'Neuquén', frecuencia: 'diario'),
-    Eventos(titulo: 'Cuestionario 3', hora: '20:00', lugar: 'Neuquén', frecuencia: 'semanal'),
-    Eventos(titulo: 'Tomar pastilla azul', hora: '20:15', lugar: 'Neuquén', frecuencia: 'diario'),
-    Eventos(titulo: 'Tomar pastilla roja', hora: '20:15', lugar: 'Neuquén', frecuencia: 'semanal'),
-  ];*/
-  //Crear objetos días
-  /*Dia objDia1 = Dia(fecha: 'Mar, 6 OCT', eventos: [
-    Eventos(titulo: 'Cuestionario 1', hora: '19:30', lugar: 'Neuquén', frecuencia: 'diario'),
-    Eventos(titulo: 'Cuestionario 2', hora: '19:45', lugar: 'Neuquén', frecuencia: 'diario'),
-    Eventos(titulo: 'Cuestionario 3', hora: '20:00', lugar: 'Neuquén', frecuencia: 'semanal'),
-    Eventos(titulo: 'Tomar pastilla azul', hora: '20:15', lugar: 'Neuquén', frecuencia: 'diario'),
-  ]);
-  Dia objDia2 =Dia(fecha: 'Mier, 7 OCT', eventos:[
-    Eventos(titulo: 'Cuestionario 4', hora: '19:30', lugar: 'Neuquén', frecuencia: 'diario'),
-    Eventos(titulo: 'Cuestionario 5', hora: '19:45', lugar: 'Neuquén', frecuencia: 'diario'),
-    Eventos(titulo: 'Cuestionario 6', hora: '20:00', lugar: 'Neuquén', frecuencia: 'semanal'),
-    Eventos(titulo: 'Tomar pastilla azul', hora: '20:15', lugar: 'Neuquén', frecuencia: 'diario'),
-  ]);*/
-  //Lista de días
 
   @override
   Widget build(BuildContext context) {
@@ -44,62 +33,66 @@ class _PantallaCalendarioState extends State<PantallaCalendario> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
+          /*Botón para ir a CalendarioExpandido. Despues hay que ver si esto lo incluimos directamente
+            en el widget calendario o si lo reemplazamos or un GestureDetector para que al hacer click
+            o doble click en el calendario te lleve a la otra página
+             */
+          FlatButton(
+            onPressed: () async {
+              Route route =MaterialPageRoute(builder: (bc) => CrearEvento());
+              dynamic result = await Navigator.of(context).push(route);
+              nuevo=result;
+              if(nuevo!=null){
+                setState(() {
+                  calendario=Calendario(add: true, fechaYhora: nuevo);
+                });
+              }
+
+            },
+            color: Colors.blue[300],
+            child: Text('Crear evento presonalizado',
+              style: TextStyle(),),
+          ),
+          /*
+            Esta fila contiene cuatro botones, son los que se usan actualmente para crear y eliminar nuevos eventos
+             */
+          /*
+            Lo que se hace es crear un nuevo objeto calendario y asignarselo a la variable de antes.
+            "remove", "add" y "elDia" son atributos de la clase calendario, que se pueden pasar al crear el objeto.
+            Si vos reemplazas un Stateful Widget por otro igual, el estado del que destruiste se pasa al nuevo,
+            es por eso que se crean más eventos y no se reemplazan por uno nuevo
+            Se repite en los cuatro botones
+            */
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Column(
-                children: [
-                  FlatButton(
-                    onPressed: (){
-                      setState(() {
-                        calendario=Calendario(remove: true, elDia: '8 de Marzo',);
-                      });
-                    },
-                    color: Colors.red,
-                    child: Text('borrar 8 M'),
-                  ),
-                  SizedBox(height: 10,),
-                  FlatButton(
-                    onPressed: (){
-                      setState(() {
-                        calendario=Calendario(remove: true, elDia: '5 de Marzo',);
-                      });
-                    },
-                    color: Colors.red,
-                    child: Text('borrar 5 M'),
-                  ),
-                ],
+              FlatButton(
+                onPressed: ({ titulo: 'Evento agregado', hora:'7:00',lugar: 'Cipolletti', frecuencia: 'semanal'}){
+                  setState(() {
+                    //Acá está lo que te decía de crear y reemplazar
+                    calendario=Calendario(add: true, fechaYhora: DateTime(2020,3,8,17,30),);
+                  });
+                },
+                color: Colors.green,
+                child: Text('agregar 8 M'),
               ),
               SizedBox(width: 10,),
-              Column(
-                children: [
-                  FlatButton(
-                    onPressed: ({ titulo: 'Evento agregado', hora:'7:00',lugar: 'Cipolletti', frecuencia: 'semanal'}){
-                      setState(() {
-                        calendario=Calendario(add: true, elDia: '8 de Marzo',);
-                      });
-                    },
-                    color: Colors.green,
-                    child: Text('agregar 8 M'),
-                  ),
-                  SizedBox(height: 10,),
-                  FlatButton(
-                    onPressed: ({ titulo: 'Evento agregado', hora:'7:00',lugar: 'Cipolletti', frecuencia: 'semanal'}){
-                      setState(() {
-                        calendario=Calendario(add: true, elDia: '5 de Marzo',);
-                      });
-                    },
-                    color: Colors.green,
-                    child: Text('agregar 5 M'),
-                  ),
-                ],
+              FlatButton(
+                onPressed: ({ titulo: 'Evento agregado', hora:'7:00',lugar: 'Cipolletti', frecuencia: 'semanal'}){
+                  setState(() {
+                    //Acá está lo que te decía de crear y reemplazar
+                    calendario=Calendario(add: true, fechaYhora: DateTime(2020,3,5,12,30),);
+                  });
+                },
+                color: Colors.green,
+                child: Text('agregar 5 M'),
               ),
             ],
           ),
+          //Se muestra el objeto calendario creado al principio
           calendario,
         ],
       ),
-
     );
   }
 }
