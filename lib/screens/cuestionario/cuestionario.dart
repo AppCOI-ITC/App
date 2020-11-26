372513e8d29af5701d2a2a30cfd2d2a72c1269c8
IrilRocco <irilrocco@gmail.com>
2020-11-26 20:09:07 -0300

    Se implementó el consejo a los pacientes y la primera parte de alarmas

--- lib/screens/cuestionario/cuestionario.dart
+++ lib/screens/cuestionario/cuestionario.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_app/models/plantillas.dart';
+import 'package:flutter_app/screens/home/home.dart';
 import 'package:flutter_app/shared/loading.dart';
 import 'package:flutter_app/custom_icons_icons.dart';
+
 DocumentReference ref;
 
 class Cuestionario extends StatefulWidget {
@@ -14,11 +16,13 @@ class Cuestionario extends StatefulWidget {
 }
 
 class _CuestionarioState extends State<Cuestionario> {
-  final CollectionReference collectionBD = FirebaseFirestore.instance.collection('DataUsuarios');
+  final CollectionReference collectionBD =
+      FirebaseFirestore.instance.collection('DataUsuarios');
   final FirebaseAuth auth = FirebaseAuth.instance;
   User user;
-  get uid => auth.currentUser.uid; 
-  Future<List<dynamic>> preguntas = FirebaseFirestore.instance.collection('Preguntas')
+  get uid => auth.currentUser.uid;
+  Future<List<dynamic>> preguntas = FirebaseFirestore.instance
+      .collection('Preguntas')
       .doc('#001')
       .get()
       .then((docSnap) {
@@ -29,10 +33,10 @@ class _CuestionarioState extends State<Cuestionario> {
     return preguntas;
   });
 
-  initUser() async { 
+  initUser() async {
     user = auth.currentUser;
   }
-  
+
   bool cuestionarioF = false;
   int index = 0;
   final reset = <bool>[false, false, false, false, false, false, false];
@@ -40,16 +44,108 @@ class _CuestionarioState extends State<Cuestionario> {
   List<int> respuestasCuest = List.filled(30, -1);
 
   Widget build(BuildContext context) {
-
-    Future<String> recuperar() async{
+    Future<String> recuperar() async {
       String out;
       out = await collectionBD.doc(uid).get().then((value) {
         var dni = value['documento'];
         var nombre = value['nombre'];
-        var outInt = dni.toString()+"-"+nombre[0]; 
+        var outInt = dni.toString() + "-" + nombre[0];
         return outInt;
       });
       return out;
+    }
+
+    void analisisRespuesta() {
+      String alarma = "El paciente XXXX presenta los siguientes síntomas:";
+      String mensaje = "Se le recomienda: ";
+      bool alarmaPresente = false;
+      bool mensajePresente = false;
+      if (respuestasCuest[7] >= 3) {
+        alarma += "\n\n Disnea";
+        alarmaPresente = true;
+      }
+      if (respuestasCuest[8] >= 3 ||
+          respuestasCuest[17] >= 2 ||
+          respuestasCuest[18] >= 2) {
+        alarma += "\n\n Dolor";
+        alarmaPresente = true;
+      }
+      if (respuestasCuest[9] == 4 || respuestasCuest[10] == 4) {
+        alarma += "\n\n Insomnio";
+        alarmaPresente = true;
+      }
+      if (respuestasCuest[11] >= 3 ||
+          respuestasCuest[12] >= 3 ||
+          respuestasCuest[13] >= 3) {
+        alarma += "\n\n Náuseas";
+        alarmaPresente = true;
+      }
+      if (respuestasCuest[14] >= 2) {
+        alarma += "\n\n Vómitos";
+        alarmaPresente = true;
+        mensaje +=
+            "\n\n Tomar quince (15) gotas de reliveran o un (1) comprimido de ondansetron";
+        mensajePresente = true;
+      }
+      if (respuestasCuest[15] >= 2) {
+        mensaje +=
+            "\n\n Tomar abundante líquido, intentar moverse más, comer más verduras y frutas, menos queso y harinas";
+        mensajePresente = true;
+        if (respuestasCuest[15] == 4) {
+          alarma += "\n\n Constipación";
+          alarmaPresente = true;
+        }
+      }
+      if (respuestasCuest[16] >= 2) {
+        mensaje +=
+            "\n\n Comer arroz o polenta con queso, evitar comidas ricas en grasas y cosas que aumentan la diarrea como salsas, frutas o jugos";
+        mensajePresente = true;
+        if (respuestasCuest[16] == 4) {
+          alarma += "\n\n Diarrea";
+          alarmaPresente = true;
+        }
+      }
+      for (int i = 19; i < 27; i++) {
+        if (respuestasCuest[i] == 4) {
+          alarma += "\n\n Psicológica";
+          alarmaPresente = true;
+        }
+      }
+      showDialog(
+          //Se va a modificar en un futuro
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('¡Cuestionario subido exitosamente!',
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 0x14, 0x53, 0x9A),
+                      fontWeight: FontWeight.bold)),
+              content: Text(
+                  (alarmaPresente ? alarma : "") +
+                      "\n\n" +
+                      (mensajePresente ? mensaje : ""),
+                  style:
+                      TextStyle(color: Color.fromARGB(255, 0x14, 0x53, 0x9A))),
+              actions: [
+                RaisedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.pop(context);
+                    },
+                    child: Text('Cerrar'))
+              ],
+            );
+          });
+    }
+
+    void subirRespuestas() async {
+      String dniN = await recuperar();
+      FirebaseFirestore.instance
+          .collection('Respuestas')
+          .doc(dniN)
+          .update(creadorMaps(respuestasCuest));
+      print(respuestasCuest);
+      analisisRespuesta();
     }
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -59,39 +155,46 @@ class _CuestionarioState extends State<Cuestionario> {
       respuestas = ["1", "2", "3", "4", "5", "6", "7"];
       //if (cant == 4) respuestas = ["En absoluto", "Un poco", "Bastante", "Mucho"];
       //else respuestas = ["1", "2", "3", "4", "5", "6", "7"];
-      
+
       for (int i = 0; i < cant; i++) {
-        listings.add(
-          Padding(
-            //10
-            padding: EdgeInsets.all(2.5),
-            child: SizedBox.fromSize(
+        listings.add(Padding(
+          //10
+          padding: EdgeInsets.all(2.5),
+          child: SizedBox.fromSize(
               //40,40
               size: Size(50, 50),
               child: MaterialButton(
-                elevation: 0.0,
-                shape: CircleBorder(side: BorderSide(width: 1.5,color: Color.fromARGB(255, 0x14, 0x53, 0x9A))),
-                textColor: !isSelected[i] ? Color.fromARGB(255, 0x14, 0x53, 0x9A) : Colors.white,
-                color: !isSelected[i] ? Colors.black.withOpacity(0.05) : Color.fromARGB(255, 0x14, 0x53, 0x9A),
-                child: Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: Text(respuestas[i],textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25.0),),
-                ),
-                onPressed: () {
-                  setState(() {
-                    isSelected.setAll(0, reset);
-                    isSelected[i] = true;
-                  });
-                  respuestasCuest[index] = i;
-                }
-              )
-            ),
-          )
-        );
+                  elevation: 0.0,
+                  shape: CircleBorder(
+                      side: BorderSide(
+                          width: 1.5,
+                          color: Color.fromARGB(255, 0x14, 0x53, 0x9A))),
+                  textColor: !isSelected[i]
+                      ? Color.fromARGB(255, 0x14, 0x53, 0x9A)
+                      : Colors.white,
+                  color: !isSelected[i]
+                      ? Colors.black.withOpacity(0.05)
+                      : Color.fromARGB(255, 0x14, 0x53, 0x9A),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Text(
+                      respuestas[i],
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 25.0),
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      isSelected.setAll(0, reset);
+                      isSelected[i] = true;
+                    });
+                    respuestasCuest[index] = i + 1;
+                  })),
+        ));
       }
       return listings;
     }
-
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -110,33 +213,33 @@ class _CuestionarioState extends State<Cuestionario> {
                 appBar: Plantillas().formatoAppBar("Cuestionario"),
                 body: Center(
                   child: AspectRatio(
-                  aspectRatio: 2/3,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: SizedBox(
-                          child: Text(
-                            data[index].split('-')[0], 
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 0x14, 0x53, 0x9A),
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
+                    aspectRatio: 2 / 3,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: SizedBox(
+                            child: Text(
+                              data[index].split('-')[0],
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 0x14, 0x53, 0x9A),
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.center,
                             ),
-                            textAlign: TextAlign.center,
                           ),
                         ),
-                      ),
-                      FittedBox(
-                        //height: 300.0,
-                        fit: BoxFit.fitWidth,
-                        child: Row(
+                        FittedBox(
+                          //height: 300.0,
+                          fit: BoxFit.fitWidth,
+                          child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: crearBotones(cant),
+                          ),
                         ),
-                      ),
                       ],
                     ),
                   ),
@@ -144,8 +247,7 @@ class _CuestionarioState extends State<Cuestionario> {
                 bottomNavigationBar: BottomAppBar(
                   elevation: 0.0,
                   color: Color.fromARGB(255, 0xFF, 0xED, 0xE1),
-                  child: cuestionarioF ? 
-                  Column(
+                  child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       Padding(
@@ -158,107 +260,53 @@ class _CuestionarioState extends State<Cuestionario> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          IconButton(icon:  Icon(CustomIcons.flecha1), onPressed: (){setState(() {
-                          if(index-1 > -1) {
-                            index--;
-                            isSelected.setAll(0, reset);
-                            if (respuestasCuest[index] != -1) {
-                                            isSelected[respuestasCuest[index]] =
-                                                true;
-                            }
-                          }
-                        });}),
                           IconButton(
-                            
-                            icon: Icon(Icons.arrow_upward), 
-                            onPressed: () async {
-                              String dniN = await recuperar();
-                              FirebaseFirestore.instance
-                              .collection('Respuestas')
-                              .doc(dniN)    
-                              .update(creadorMaps(respuestasCuest));
-                              print(respuestasCuest);
-                            }
-                            // FirebaseFirestore.instance
-                            //     .collection('Respuestas')
-                            //     .doc(dniN)
-                            //     .get()
-                            //     .then((doc) {
-                            //   if (doc.exists) {
-                            //     FirebaseFirestore.instance
-                            //         .collection('Respuestas')
-                            //         .doc(dniN)
-                            //         .update(
-                            //             creadorMaps(respuestasCuest));
-                            //   } else {
-                            //     FirebaseFirestore.instance
-                            //         .collection('Respuestas')
-                            //         .doc(dniN)
-                            //         .set(
-                            //             creadorMaps(respuestasCuest));
-                            //   }
+                              icon: Icon(CustomIcons.flecha1),
+                              onPressed: () {
+                                setState(() {
+                                  if (index - 1 > -1) {
+                                    index--;
+                                    isSelected.setAll(0, reset);
+                                    if (respuestasCuest[index] != -1) {
+                                      isSelected[respuestasCuest[index] - 1] =
+                                          true;
+                                    }
+                                  }
+                                });
+                              }),
+                          RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0)),
+                            child: Text("Subir cuestionario",
+                                style: TextStyle(
+                                    color:
+                                        Color.fromARGB(255, 0x14, 0x53, 0x9A),
+                                    fontWeight: FontWeight.bold)),
+                            onPressed:
+                                cuestionarioF ? () => subirRespuestas() : null,
                           ),
                           IconButton(
-                            icon: Icon(CustomIcons.flecha), 
-                            onPressed: (){setState(() {
-                              if(index+1 < 30) {
-                                index++;
-                                if(respuestasCuest[index] != -1){
-                                  isSelected.setAll(0, reset);
-                                  isSelected[respuestasCuest[index]] = true;
-                                }else{
-                                  isSelected.setAll(0, reset);
-                                }  
-                              }
-                              if((index==29) && (respuestasCuest.indexOf(-1) == -1)) cuestionarioF = true;
-                            });}
-                          )
+                              icon: Icon(CustomIcons.flecha),
+                              onPressed: () {
+                                setState(() {
+                                  if (index + 1 < 30) {
+                                    index++;
+                                    //navegacion();
+                                    if (respuestasCuest[index] != -1) {
+                                      isSelected.setAll(0, reset);
+                                      isSelected[respuestasCuest[index] - 1] =
+                                          true;
+                                    } else {
+                                      isSelected.setAll(0, reset);
+                                    }
+                                  }
+                                  if ((index == 29) &&
+                                      (respuestasCuest.indexOf(-1) == -1))
+                                    cuestionarioF = true;
+                                });
+                              })
                         ],
-                      ) 
-                    ],
-                  )
-                  : 
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-                        child: Container(
-                          height: 1.2,
-                          color: Color.fromARGB(255, 0x14, 0x53, 0x9A),
-                        ),
                       ),
-                      Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        IconButton(icon:  Icon(CustomIcons.flecha1), onPressed: (){setState(() {
-                          if(index-1 > -1) {
-                            index--;
-                            isSelected.setAll(0, reset);
-                            if (respuestasCuest[index] != -1) {
-                                            isSelected[respuestasCuest[index]] =
-                                                true;
-                            }
-                          }
-                        });}),
-                        IconButton(
-                          icon: Icon(CustomIcons.flecha), 
-                          onPressed: (){setState(() {
-                            if(index+1 < 30) {
-                              index++;
-                              //navegacion();
-                              if(respuestasCuest[index] != -1){
-                                isSelected.setAll(0, reset);
-                                isSelected[respuestasCuest[index]] = true;
-                              }else{
-                                isSelected.setAll(0, reset);
-                              }  
-                            }
-                            if((index==29) && (respuestasCuest.indexOf(-1) == -1)) cuestionarioF = true;
-                          });}
-                        )
-                      ],
-                    ),
                     ],
                   ),
                 ),
@@ -268,12 +316,14 @@ class _CuestionarioState extends State<Cuestionario> {
           } catch (e) {
             snapshot.hasError ? Text("Error de data") : null;
           }
-    });
+        });
   }
 }
 
 Map creadorMaps(List<int> lista) {
   String fecha = DateTime.now().toIso8601String();
-  Map<String, dynamic> map = {"${fecha.substring(0,16).replaceAll('T', '_')}": lista};
+  Map<String, dynamic> map = {
+    "${fecha.substring(0, 16).replaceAll('T', '_')}": lista
+  };
   return map;
 }
